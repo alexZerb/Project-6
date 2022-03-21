100e005ba600c7d86231b8a6e5183831ca4e74ac
Alex Szczerba <Szczerba18@icloud.com>
2022-03-20 18:52:31 -0700

    Update
    Re worked JSON data, fixed photos so that they can be accessed in public file, completed pug templates

--- app.js
+++ app.js
@@ -4,16 +4,33 @@ const app = express();
 
 const { projects } = require('./data.json');
 
-app.set('view engine', 'pug');
-
 app.use(express.json());
 
-app.use('static', express.static('public'));
+app.set('view engine', 'pug');
+app.use('/static', express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', (req, res, next) => {
+    res.render('index', {projects});
 })
 
-app.listen(3000, () => {
-    console.log('The application is running on localhost: 3000!');
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.get('/projects/:id', (req, res, next) => {
+    const id = req.params.id;
+    const project = projects[id];
+        if(project) {
+            res.render('project', { project });
+        } else {
+            const err = new Error;
+            err.status = 404;
+            err.message = `Cannot find project ${id}`;
+            next(err);
+        }
+
+});
+const port = process.PORT || 3000;
+app.listen(port, () => {
+    console.log(`The application is running on localhost: ${port}`);
 });
