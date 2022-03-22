d1c52f6a1bd3b4f350471f9ad2d25a77ba4372b8
Alex Szczerba <Szczerba18@icloud.com>
2022-03-22 13:07:33 -0700

    update

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
+// require express and data.json 
 const express = require('express');
-const { projects } = require('./data.json');
 const app = express();
 
-
-
+const {projects} = require('./data.json');
 app.use(express.json());
 
+
+// Middleware and View engine set to pug
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
+// Routing for index, about, and projects
+
 app.get('/', (req, res, next) => {
-    res.render('index', {projects});
-})
+    res.render('index', { projects })
+});
 
 app.get('/about', (req, res) => {
     res.render('about');
-})
+});
 
 app.get('/projects/:id', (req, res, next) => {
-    const id = req.params.id;
-    const project = projects[id];
-        if(project) {
-            res.render('project', { project });
-        } else {
-            const err = new Error;
-            err.status = 404;
-            err.message = `Cannot find project ${id}`;
-            next(err);
-        }
-
+    
+    res.render('project', { id: parseInt(req.params.id), data: data.projects });
 });
+
+//  404 Error Handling
+app.use(( req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+//Set app to listen on port 3000 
+
 const port = process.PORT || 3000;
 app.listen(port, () => {
     console.log(`The application is running on localhost: ${port}`);
